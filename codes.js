9d2cf0349455683de970b805caa859004df967f1
Jorge Betancourt <jjbetan98@gmail.com>
2016-08-25 18:58:56 -0400

    Add button to display congrats message

--- codes.js
+++ codes.js
@@ -3,4 +3,6 @@ function button() {
   	var number2 = document.getElementById('Number2').value;
 	var out = parseFloat(number1) * parseFloat(number2);
 	document.getElementById('output').innerText = out;
+	var message = "Yay I did it!";
+	document.getElementByID('MessageBox').innerText = message;
 }
